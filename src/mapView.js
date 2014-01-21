GNOME/gnome-maps

commit 85b4fc69a6d1d95b4fef8ad94463f76c5c033958

MapLocation: Re-add easing on goTo
This patch re-adds easing on mapLocation.goTo for a much smoother
transition.

https://bugzilla.gnome.org/show_bug.cgi?id=722350

src/mapView.js
@@ -63,7 +63,7 @@ const MapView = new Lang.Class({
         this.view = this.actor;
         this.view.set_zoom_level(3);
         this.view.min_zoom_level = MapMinZoom;
-        this.view.goto_animation_mode = Clutter.AnimationMode.LINEAR;
+        this._view.goto_animation_mode = Clutter.AnimationMode.EASE_IN_OUT_CUBIC;
         this.view.set_reactive(true);
 
         this.view.connect('notify::latitude', this._onViewMoved.bind(this));

src/mapLocation.js
@@ -67,12 +67,15 @@ const MapLocation = new Lang.Class({
          * location.
          */
 
+        this._view.goto_animation_mode = Clutter.AnimationMode.EASE_IN_CUBIC;
         Utils.once(this._view, "animation-completed", (function() {
             Utils.once(this._view, "animation-completed::go-to", (function() {
                 this.zoomToFit();
+                this._view.goto_animation_mode = Clutter.AnimationMode.EASE_IN_OUT_CUBIC;
                 this.emit('gone-to');
             }).bind(this));
 
+            this._view.goto_animation_mode = Clutter.AnimationMode.EASE_OUT_CUBIC;
             this._view.go_to(this.latitude, this.longitude);
         }).bind(this));
 

commit aaa9d10d141847d831791ca53d0dcadad3be397a

Fix a crash on start bug
Fix stupid bug introduced by me in 85b4fc6.

src/mapView.js
@@ -63,7 +63,7 @@ const MapView = new Lang.Class({
         this.view = this.actor;
         this.view.set_zoom_level(3);
         this.view.min_zoom_level = MapMinZoom;
-        this._view.goto_animation_mode = Clutter.AnimationMode.EASE_IN_OUT_CUBIC;
+        this.view.goto_animation_mode = Clutter.AnimationMode.EASE_IN_OUT_CUBIC;
         this.view.set_reactive(true);
 
         this.view.connect('notify::latitude', this._onViewMoved.bind(this));