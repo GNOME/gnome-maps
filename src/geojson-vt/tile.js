289f361a4bc0be77baad08c9b16b6cae4f413b4b
Jonas Danielsson <jonas@threetimestwo.org>
2015-10-23 12:18:39 +0200

    Add geojson-vt from Mapbox
    Geojson-vt is A highly efficient JavaScript library for slicing GeoJSON data
    into vector tiles on the fly, primarily designed to enable rendering and
    interacting with large geospatial datasets on the browser side
    (without a server).
    
    https://github.com/mapbox/geojson-vt, see LICENSE for license.

---
+++ src/geojson-vt/tile.js
@@ -0,0 +1,85 @@
+'use strict';
+
+module.exports = createTile;
+
+function createTile(features, z2, tx, ty, tolerance, noSimplify) {
+    var tile = {
+        features: [],
+        numPoints: 0,
+        numSimplified: 0,
+        numFeatures: 0,
+        source: null,
+        x: tx,
+        y: ty,
+        z2: z2,
+        transformed: false,
+        min: [2, 1],
+        max: [-1, 0]
+    };
+    for (var i = 0; i < features.length; i++) {
+        tile.numFeatures++;
+        addFeature(tile, features[i], tolerance, noSimplify);
+
+        var min = features[i].min,
+            max = features[i].max;
+
+        if (min[0] < tile.min[0]) tile.min[0] = min[0];
+        if (min[1] < tile.min[1]) tile.min[1] = min[1];
+        if (max[0] > tile.max[0]) tile.max[0] = max[0];
+        if (max[1] > tile.max[1]) tile.max[1] = max[1];
+    }
+    return tile;
+}
+
+function addFeature(tile, feature, tolerance, noSimplify) {
+
+    var geom = feature.geometry,
+        type = feature.type,
+        simplified = [],
+        sqTolerance = tolerance * tolerance,
+        i, j, ring, p;
+
+    if (type === 1) {
+        for (i = 0; i < geom.length; i++) {
+            simplified.push(geom[i]);
+            tile.numPoints++;
+            tile.numSimplified++;
+        }
+
+    } else {
+
+        // simplify and transform projected coordinates for tile geometry
+        for (i = 0; i < geom.length; i++) {
+            ring = geom[i];
+
+            // filter out tiny polylines & polygons
+            if (!noSimplify && ((type === 2 && ring.dist < tolerance) ||
+                                (type === 3 && ring.area < sqTolerance))) {
+                tile.numPoints += ring.length;
+                continue;
+            }
+
+            var simplifiedRing = [];
+
+            for (j = 0; j < ring.length; j++) {
+                p = ring[j];
+                // keep points with importance > tolerance
+                if (noSimplify || p[2] > sqTolerance) {
+                    simplifiedRing.push(p);
+                    tile.numSimplified++;
+                }
+                tile.numPoints++;
+            }
+
+            simplified.push(simplifiedRing);
+        }
+    }
+
+    if (simplified.length) {
+        tile.features.push({
+            geometry: simplified,
+            type: type,
+            tags: feature.tags || null
+        });
+    }
+}
