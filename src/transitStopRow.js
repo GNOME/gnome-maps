d1cf80cb937418fe3e8ae7161477f19f9f7c1473
Marcus Lundblad <ml@update.uu.se>
2016-06-29 22:59:13 +0200

    Add list box row class for transit stops
    List box row showing intermediate stops.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=755808

---
+++ src/transitStopRow.js
@@ -0,0 +1,47 @@
+/* -*- Mode: JS2; indent-tabs-mode: nil; js2-basic-offset: 4 -*- */
+/* vim: set et ts=4 sw=4: */
+/*
+ * Copyright (c) 2017 Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
+const Lang = imports.lang;
+
+const _ = imports.gettext.gettext;
+
+const Gtk = imports.gi.Gtk;
+
+const TransitStopRow = new Lang.Class({
+    Name: 'TransitStopRow',
+    Extends: Gtk.ListBoxRow,
+    Template: 'resource:///org/gnome/Maps/ui/transit-stop-row.ui',
+    InternalChildren: [ 'nameLabel',
+                        'timeLabel' ],
+
+    _init: function(params) {
+        this.stop = params.stop;
+        delete params.stop;
+
+        this._final = params.final;
+        delete params.final;
+
+        this.parent(params);
+
+        this._nameLabel.label = this.stop.name;
+        this._timeLabel.label = this.stop.prettyPrint({ isFinal: this._final });
+    }
+});
