222fb8422b14280a590405fee5748294749a4a79
Marcus Lundblad <ml@update.uu.se>
2017-11-09 22:39:14 +0100

    mainWindow: Don't activate aerial view when unavailable
    Closes #103

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -482,8 +482,11 @@ var MainWindow = GObject.registerClass({
     }
 
     _onAerialViewActivate() {
-        this._mapView.setMapType(MapView.MapType.AERIAL);
-        this.layersPopover.setMapType(MapView.MapType.AERIAL);
+        // don't attempt to switch to aerial if we don't have tiles for it
+        if (Service.getService().tiles.aerial) {
+            this._mapView.setMapType(MapView.MapType.AERIAL);
+            this.layersPopover.setMapType(MapView.MapType.AERIAL);
+        }
     }
 
     _onToggleSidebarChangeState(action, variant) {
