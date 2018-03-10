0b0d2a4b18ec109955cdb44d951328db4008e2d3
Marcus Lundblad <ml@update.uu.se>
2018-03-10 22:14:12 +0100

    osmOauthProxyCall: Use GPL 2+
    All other files in Maps are licensed as GPL 2+, marking this
    as 3+ was a mistake, and since I'm the only contributer to
    these files, I should be able to safely change them (and
    they could still be optionally considered 3+ by some
    potential third-party derivative work).

--- lib/maps-osm-oauth-proxy-call.h
+++ lib/maps-osm-oauth-proxy-call.h
@@ -1,19 +1,18 @@
-/* maps-osm-oauth-proxy-call.h
+/*
+ * Copyright (c) 2015 Marcus Lundblad
  *
- * Copyright (C) 2015 Marcus Lundblad <ml@update.uu.se>
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>
  *
  * Author: Marcus Lundblad <ml@update.uu.se>
  */
