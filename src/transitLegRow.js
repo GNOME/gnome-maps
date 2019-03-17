30ce1fbb65da1a8ab3f1fc1f853b066c1927bef1
Marcus Lundblad <ml@update.uu.se>
2019-03-17 22:09:09 +0100

    transitLegRow: Use utility function to print from label

--- src/transitLegRow.js
+++ src/transitLegRow.js
@@ -28,6 +28,7 @@ const Gtk = imports.gi.Gtk;
 const Pango = imports.gi.Pango;
 
 const InstructionRow = imports.instructionRow;
+const Transit = imports.transit;
 const TransitRouteLabel = imports.transitRouteLabel;
 const TransitStopRow = imports.transitStopRow;
 const Utils = imports.utils;
@@ -63,22 +64,7 @@ var TransitLegRow = GObject.registerClass({
         super._init(params);
 
         this._modeImage.icon_name = this._leg.iconName;
-        if (this._start) {
-            if (this._leg.from) {
-                /* Translators: this is a format string indicating instructions
-                 * starting a journey at the address given as the parameter
-                 */
-                this._fromLabel.label = _("Start at %s").format(this._leg.from);
-            } else {
-                /* Translators: this indicates starting a journey at a location
-                 * with no set name (such as when the user started routing from
-                 * an arbitrary point on the map)
-                 */
-                this._fromLabel.label = _("Start");
-            }
-        } else {
-            this._fromLabel.label = this._leg.from;
-        }
+        this._fromLabel.label = Transit.getFromLabel(this._leg, this._start);
 
         if (this._leg.transit) {
             let routeLabel = new TransitRouteLabel.TransitRouteLabel({
