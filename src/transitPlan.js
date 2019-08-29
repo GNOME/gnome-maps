fc49ce939eedac1bdccf33ad5102962c38612c22
Marcus Lundblad <ml@update.uu.se>
2019-08-29 21:21:50 +0200

    transitPlan: Add properties for attribution
    Add properties for attribution name
    and URL.

--- src/transitPlan.js
+++ src/transitPlan.js
@@ -96,6 +96,8 @@ var Plan = GObject.registerClass({
     _init(params) {
         super._init(params);
         this.reset();
+        this._attribution = null;
+        this._attributionUrl = null;
     }
 
     get itineraries() {
@@ -104,6 +106,22 @@ var Plan = GObject.registerClass({
 
     get selectedItinerary() {
         return this._selectedItinerary;
+    }
+
+    get attribution() {
+        return this._attribution;
+    }
+
+    set attribution(attribution) {
+        this._attribution = attribution;
+    }
+
+    get attributionUrl() {
+        return this._attributionUrl;
+    }
+
+    set attributionUrl(attributionUrl) {
+        this._attributionUrl = attributionUrl;
     }
 
     update(itineraries) {
@@ -116,6 +134,8 @@ var Plan = GObject.registerClass({
         this._itineraries = [];
         this.bbox = null;
         this._selectedItinerary = null;
+        this._attribution = null;
+        this._attributionUrl = null;
         this.emit('reset');
     }
 
