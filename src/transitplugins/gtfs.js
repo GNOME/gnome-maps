96ce73c3c8f29c99cd3e6416cb38ef1c0714ba80
Marcus Lundblad <ml@update.uu.se>
2020-02-19 23:10:55 +0100

    WIP: Add transit plugin doing local routing on dowloaded GTFS

---
+++ src/transitplugins/gtfs.js
@@ -0,0 +1,40 @@
+/* -*- Mode: JS2; indent-tabs-mode: nil; js2-basic-offset: 4 -*- */
+/* vim: set et ts=4 sw=4: */
+/*
+ * Copyright (c) 2020 Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
+const Maps = imports.gi.GnomeMaps;
+
+const Utils = imports.utils;
+
+var GTFS = class GTFS {
+    constructor(params) {
+        // TODO: for now just read files from an unzipped GTFS file at path…
+        this._gtfs = Maps.GTFS.new(params.name);
+    }
+
+    fetchFirstResults() {
+        let extract = this._gtfs.parse();
+
+    }
+
+    fetchMoreResults() {
+
+    }
+}
