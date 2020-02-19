ef0859bf1432b9d8d8e048b41d701f5f6fb94198
Marcus Lundblad <ml@update.uu.se>
2020-02-19 23:10:55 +0100

    WIP: Add transit plugin doing local routing on dowloaded GTFS

---
+++ src/transitplugins/gtfs.js
@@ -0,0 +1,54 @@
+/* -*- Mode: JS2; indent-tabs-mode: nil; js2-basic-offset: 4 -*- */
+/* vim: set et ts=4 sw=4: */
+/*
+ * Copyright (c) 2020 Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
+const Maps = imports.gi.GnomeMaps;
+
+const Application = imports.application;
+const Utils = imports.utils;
+
+var GTFS = class GTFS {
+    constructor(params) {
+        // TODO: for now just read files from an unzipped GTFS file at path…
+        this._gtfs = Maps.GTFS.new(params.name);
+        this._query = Application.routeQuery;
+        Utils.debug('end constructor');
+    }
+
+    fetchFirstResults() {
+        let filledPoints = this._query.filledPoints;
+        let extract = this._gtfs.parse();
+        let startLat = filledPoints[0].place.location.latitude;
+        let startLon = filledPoints[0].place.location.longitude;
+        let endLat = filledPoints.last().place.location.latitude;
+        let endLon = filledPoints.last().place.location.longitude;
+
+        Utils.debug('stops near start');
+        let startStops = this._gtfs.get_nearby_stops(startLat, startLon, 2000);
+
+        Utils.debug('stops near end');
+        let endStops = this._gtfs.get_nearby_stops(endLat, endLon, 2000);
+
+    }
+
+    fetchMoreResults() {
+
+    }
+}
