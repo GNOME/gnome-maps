2d793f4b4ea59426c02dce37efbb529dd57916fa
Marcus Lundblad <ml@update.uu.se>
2020-02-27 23:50:12 +0100

    WIP: Add class representing a trip in a GTFS feed

--- lib/maps-gtfs-route.c
+++ lib/maps-gtfs-route.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
 #include "maps-gtfs-route.h"
 
 typedef struct
