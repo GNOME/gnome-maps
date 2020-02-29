03c5731d0a4597967f916a0bd60a55aec6804832
Marcus Lundblad <ml@update.uu.se>
2020-02-29 22:12:43 +0100

    WIP: Add class representing a stop time in a GTFS feed

---
+++ lib/maps-gtfs-stop-time.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define MAPS_TYPE_GTFS_STOP_TIME (maps_gtfs_stop_time_get_type())
+
+G_DECLARE_FINAL_TYPE (MapsGTFSStopTime, maps_gtfs_stop_time, MAPS, GTFS_STOP_TIME, GObject)
+
+MapsGTFSStopTime *maps_gtfs_stop_time_new (void);
+
+G_END_DECLS
