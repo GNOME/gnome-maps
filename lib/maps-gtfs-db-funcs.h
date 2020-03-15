0f6247aa2255166fa9a494f8421c5443681e1740
Marcus Lundblad <ml@update.uu.se>
2020-03-15 23:26:45 +0100

    WIP: Add custom SQLite geo util functions

---
+++ lib/maps-gtfs-db-funcs.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
+#pragma once
+
+#include <sqlite3.h>
+
+void maps_gtfs_db_funcs_init (sqlite3 *db);
+
