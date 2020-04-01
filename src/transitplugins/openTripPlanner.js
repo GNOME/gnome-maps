514b3875caa015204f20f2f6997ff5224d27e5cf
Marcus Lundblad <ml@update.uu.se>
2020-04-01 20:15:06 +0200

    openTripPlanner: Don't reset query points on failure
    Allows the user to re-try the same query with a
    turn-by-turn mode.

--- src/transitplugins/openTripPlanner.js
+++ src/transitplugins/openTripPlanner.js
@@ -564,8 +564,7 @@ var OpenTripPlanner = class OpenTripPlanner {
             this._extendPrevious = false;
             this.plan.noMoreResults();
         } else {
-            this._reset();
-            this.plan.noRouteFound();
+            this.plan.noRouteFound();re
         }
     }
 
