86a443ae809670168e6fb527ad8c541cad844903
Marcus Lundblad <ml@update.uu.se>
2020-04-28 23:39:58 +0200

    WIP: Add unit tests for the utils module

---
+++ tests/utilsTest.js
@@ -0,0 +1,42 @@
+/* -*- Mode: JS2; indent-tabs-mode: nil; js2-basic-offset: 4 -*- */
+/* vim: set et ts=4 sw=4: */
+/*
+ * Copyright (c) 2020 Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
+pkg.require({ 'Gdk': '3.0',
+              'Gtk': '3.0' });
+
+const JsUnit = imports.jsUnit;
+
+const Utils = imports.utils;
+
+const Geocode = imports.gi.GeocodeGlib;
+
+function main() {
+    osmTypeToStringTest();
+}
+
+function osmTypeToStringTest() {
+    JsUnit.assertEquals('OSM type node', 'node',
+                        Utils.osmTypeToString(Geocode.PlaceOsmType.NODE));
+    JsUnit.assertEquals('OSM type way', 'way',
+                        Utils.osmTypeToString(Geocode.PlaceOsmType.WAY));
+    JsUnit.assertEquals('OSM type node', 'relation',
+                        Utils.osmTypeToString(Geocode.PlaceOsmType.RELATION));
+}
