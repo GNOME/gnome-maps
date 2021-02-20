7e12c8ed73ebb1d951ff0c099c6eeb3816659294
Marcus Lundblad <ml@update.uu.se>
2021-02-20 15:23:43 +0100

    Add constants module
    Add module with common constants, for now containing
    coordinate constrains nessesary to re-implement
    the bounding box from libchamplain.

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+/* -*- Mode: JS2; indent-tabs-mode: nil; js2-basic-offset: 4 -*- */
+/* vim: set et ts=4 sw=4: */
+/*
+ * Copyright (c) 2021, Marcus Lundblad
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Lundblad <ml@update.uu.se>
+ */
+
+/**
+ * Constraints of visible location in the standard Mercaartor projection.
+ */
+var MIN_LATITUDE = -85.0511287798;
+var MAX_LATITUDE = 85.0511287798;
+var MIN_LONGITUDE = -180;
+var MAX_LONGITUDE = 180;
