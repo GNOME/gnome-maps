bd289e5ae74b1ec0ffab1c0011334d99cae4dc1a
Marcus Lundblad <ml@dfupdate.se>
2023-01-24 23:22:37 +0100

    Add list box row to go to main POI categories
    GtkListBoxRow to put at the top of a POI
    subcategory list to enable going back
    the list of main categories.

---
+++ src/poiCategoryGobackRow.js
@@ -0,0 +1,30 @@
+/* -*- Mode: JS2; indent-tabs-mode: nil; js2-basic-offset: 4 -*- */
+/* vim: set et ts=4 sw=4: */
+/*
+ * Copyright (c) 2023 Marcus Lundblad.
+ *
+ * GNOME Maps is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * GNOME Maps is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GNOME Maps; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Lundblad <ml@dfupdate.se>
+ */
+
+import GObject from 'gi://GObject';
+import Gtk from 'gi://Gtk';
+
+export class PoiCategoryGobackRow extends Gtk.ListBoxRow {
+};
+
+GObject.registerClass({
+    Template: 'resource:///org/gnome/Maps/ui/poi-category-goback-row.ui',
+}, PoiCategoryGobackRow);
