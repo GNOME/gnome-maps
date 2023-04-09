9c015f63b60850d8bcf0084a3a5b0e3c82b07a6c
Marcus Lundblad <ml@dfupdate.se>
2023-04-09 22:34:48 +0200

    mainWindow: Add action to show last search result
    Show last search results (if available) on ctrl+R.

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -245,6 +245,10 @@ export class MainWindow extends Gtk.ApplicationWindow {
                 accels: ['<Shift><Primary>F'],
                 onActivate: () => this._onBrowseActivate()
             },
+            'show-search-results': {
+                accels: ['<Primary>R'],
+                onActivate: () => this._onShowSearchResultsActivate()
+            },
             'print-route': {
                 accels: ['<Primary>P'],
                 onActivate: () => this._printRouteActivate()
@@ -450,6 +454,13 @@ export class MainWindow extends Gtk.ApplicationWindow {
 
     _onBrowseActivate() {
         this._placeEntry.browsePois();
+    }
+
+    _onShowSearchResultsActivate() {
+        if (this._searchBar.placeEntry.popover.numResults > 0) {
+            this._searchBar.placeEntry.popover.showResult();
+            this._searchBar.placeEntry.grab_focus();
+        }
     }
 
     _onGotoUserLocationActivate() {
