1b712ba2eae44aeb471d4e5981060f4000811633
Marcus Lundblad <ml@dfupdate.se>
2024-05-10 22:51:28 +0200

    place: Add additional tags considered for the main tag
    Add addional tags to consider for the osmKey main tag.

--- src/place.js
+++ src/place.js
@@ -352,7 +352,7 @@ export class Place extends GObject.Object {
         if (osmTags) {
             const tag = [
                 'place', 'amenity', 'leisure', 'shop', 'tourism', 'highway',
-                'railway', 'aeroway', 'building'
+                'railway', 'aeroway', 'building', 'office', 'historic', 'barrier'
             ].find(key => key in osmTags);
 
             if (tag)
