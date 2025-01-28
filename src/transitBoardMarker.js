665ca072e83aecc5b5613ad9fd6bcbd1e9552adc
Marcus Lundblad <ml@dfupdate.se>
2025-01-28 22:22:15 +0100

    transitBoardMarker: Rebase on CircleIconMarker
    Extend the generic CircleIconMarker.

--- src/transitBoardMarker.js
+++ src/transitBoardMarker.js
@@ -19,42 +19,21 @@
  * Author: Marcus Lundblad <ml@update.uu.se>
  */
 
-import Adw from 'gi://Adw';
 import GObject from 'gi://GObject';
-import Graphene from 'gi://Graphene';
-import Gsk from 'gi://Gsk';
-import Gtk from 'gi://Gtk';
 
-import * as Color from './color.js';
-import {IconMarker} from './iconMarker.js';
-import {Location} from './location.js';
-import {Place} from './place.js';
-import * as TransitPlan from './transitPlan.js';
-import * as Utils from './utils.js';
+import {CircleIconMarker} from './circleIconMarker.js';
 
-const ICON_SIZE = 16;
-const MARKER_SIZE = 32;
-const OUTLINE_WIDTH = 1;
-
-/* threashhold for route color luminance when we consider it more or less
- * as white, and draw an outline around the label, and vice versa for dark mode
- */
-const OUTLINE_LUMINANCE_THREASHHOLD = 0.6;
-const DARK_OUTLINE_LUMINANCE_THREASHHOLD = 0.2;
-
-export class TransitBoardMarker extends IconMarker {
+export class TransitBoardMarker extends CircleIconMarker {
 
     constructor({leg, ...params}) {
         const firstPoint = leg.polyline[0];
-        const location = new Location({ latitude: firstPoint.latitude,
-                                        longitude: firstPoint.longitude });
 
-        super({...params, place: new Place({ location: location })});
-
-        this._leg = leg;
-        this._styleManager = Adw.StyleManager.get_default();
-        this._image.pixel_size = MARKER_SIZE;
-        this._pathBuilder = new Gsk.PathBuilder();
+        super({ latitude:   firstPoint.latitude,
+                longitude: firstPoint.longitude,
+                color:     leg.color,
+                textColor: leg.textColor,
+                iconName:  leg.iconName,
+                ...params });
     }
 
     vfunc_map() {
@@ -64,77 +43,6 @@ export class TransitBoardMarker extends IconMarker {
         this._image.paintable = this._createPaintable();
 
         super.vfunc_map();
-    }
-
-    vfunc_unmap() {
-        this._styleManager.disconnect(this._darkId);
-
-        super.vfunc_unmap();
-    }
-
-    /* Creates a Gdk.Paintable for the given transit leg, showing the
-     * corresponding transit type icon and rendered inside a circle using the
-     * foreground color of the icon taken from the transit legs text color
-     * attribute and background color taken from the transit legs color
-     * attribute.
-     * Also draw an outline in the same color as the icon in case the
-     * background color above a threshold to improve readability against the
-     * map background.
-     */
-    _createPaintable() {
-        try {
-            const bgColor = this._leg.color ??
-                            (this._styleManager.dark ?
-                             TransitPlan.DEFAULT_DARK_ROUTE_COLOR :
-                             TransitPlan.DEFAULT_ROUTE_COLOR);
-            const defaultTextColor =
-                this._styleManager.dark ? TransitPlan.DEFAULT_DARK_ROUTE_TEXT_COLOR :
-                                          TransitPlan.DEFAULT_ROUTE_TEXT_COLOR;
-            const fgColor =
-                Color.getContrastingForegroundColor(bgColor, this._leg.textColor ??
-                                                             defaultTextColor);
-            const hasOutline =
-                this._styleManager.dark ?
-                Color.relativeLuminance(bgColor) < DARK_OUTLINE_LUMINANCE_THREASHHOLD :
-                Color.relativeLuminance(bgColor) > OUTLINE_LUMINANCE_THREASHHOLD;
-            const fgRGBA = Color.parseColorAsRGBA(fgColor);
-            const bgRGBA = Color.parseColorAsRGBA(bgColor);
-            const paintable = this._paintableFromIconName(this._leg.iconName,
-                                                          ICON_SIZE, fgRGBA);
-            const snapshot = Gtk.Snapshot.new();
-            const rect = new Graphene.Rect();
-            const iconBounds = new Graphene.Rect();
-            const center = new Graphene.Point({ x: MARKER_SIZE / 2,
-                                                y: MARKER_SIZE / 2 });
-
-            rect.init(0, 0, MARKER_SIZE, MARKER_SIZE);
-            iconBounds.init((MARKER_SIZE - ICON_SIZE) / 2,
-                            (MARKER_SIZE - ICON_SIZE) / 2,
-                            ICON_SIZE, ICON_SIZE);
-
-            this._pathBuilder.add_circle(center, MARKER_SIZE / 2);
-            snapshot.append_fill(this._pathBuilder.to_path(),
-                                 Gsk.FILL_RULE_EVEN_ODD,
-                                 bgRGBA);
-
-            if (hasOutline) {
-                this._pathBuilder.add_circle(center, MARKER_SIZE / 2 -
-                                                     OUTLINE_WIDTH / 2);
-                snapshot.append_stroke(this._pathBuilder.to_path(),
-                                       new Gsk.Stroke(OUTLINE_WIDTH),
-                                       fgRGBA);
-            }
-
-            snapshot.append_texture(paintable, iconBounds);
-
-            const node = snapshot.to_node();
-            const renderer = this._mapView.get_native().get_renderer();
-
-            return renderer.render_texture(node, rect);
-        } catch (e) {
-            Utils.debug('Failed to load image: %s'.format(e.message));
-            return null;
-        }
     }
 }
 
