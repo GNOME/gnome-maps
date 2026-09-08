e2a88e15d03bc6519123104e64f3d7da795690b7
Marcus Lundblad <ml@dfupdate.se>
2026-09-08 09:10:29 +0200

    transitStopRow: Show dropoff/pickup restrictions

--- src/transitStopRow.js
+++ src/transitStopRow.js
@@ -59,6 +59,20 @@ export class TransitStopRow extends Gtk.ListBoxRow {
                 Utils.getTrackIndication(stop.track, routeType);
         }
 
+        if (intermediate && stop.alightingOnly) {
+            this._informationLabel.visible = true;
+            /* Translators: this is a label indicating no passanger boarding is
+             * allowed at a public transit stop.
+             */
+            this._informationLabel.label = _("No boarding");
+        } else if (intermediate && stop.boardingOnly) {
+            this._informationLabel.visible = true;
+             /* Translators: this is a label indicating no passanger dropoff is
+             * allowed at a public transit stop.
+             */
+            this._informationLabel.label = _("No alighting");
+        }
+
         if (!time.equal(scheduledTime)) {
             this._scheduledTimeLabel.visible = true;
             this._scheduledTimeLabel.label =
@@ -90,5 +104,6 @@ GObject.registerClass({
                         'nameLabel',
                         'trackLabel',
                         'timeLabel',
-                        'scheduledTimeLabel' ]
+                        'scheduledTimeLabel',
+                        'informationLabel' ]
 }, TransitStopRow);
